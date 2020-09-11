e007260ff3a9a98c8cffa9c5416a93edf5e97c31
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2020-09-11 17:31:06 +0100

    added all script files for the project including necessary API files

---
+++ script.js
@@ -0,0 +1,48 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+//Disable/Enable button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+//Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: '879f09a074c246b1994f10f157b32726',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+//Get jokes from Joke API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming,Dark?blacklistFlags=nsfw,religious,racist';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if(data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        //Text-to-Speech
+        tellMe(joke);
+        //Disable Button
+        toggleButton();
+    } catch (error) {
+        //Catch Errors
+        console.log('whoops', error)
+    }
+}
+
+//Event listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
